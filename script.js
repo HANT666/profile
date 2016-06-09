7b364ae274ad49226cca81dfb27b46507fc631c4
HANT666 <iywppfw@yandex.ru>
2016-06-10 00:56:00 +0500

    фокус текст поля при выборе "Другое"

--- script.js
+++ script.js
@@ -1,8 +1,18 @@
 window.onload = function(){
   var label = $("#kazna");
   label.style.background = "red";
+  //alert("LOL!");
 }
 
-function $(selector){
+/*function $(selector){
   return document.querySelector(selector);
+}*/
+
+
+function other(text_edit){
+  //alert("LOL!");
+  /*other1_text*/text_edit.focus();
+  /*var other = document.getElementById('other');
+  var other1_text = document.getElementById('other1_text');
+  other1_text.focus();*/
 }
