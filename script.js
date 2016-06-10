02b2a0c67d9dedd0b60b61c00a08b4a3de888cff
HANT666 <iywppfw@yandex.ru>
2016-06-10 05:37:31 +0500

    ползунок бюджета

--- script.js
+++ script.js
@@ -1,18 +1,32 @@
 window.onload = function(){
   var label = $("#kazna");
   label.style.background = "red";
-  //alert("LOL!");
 }
 
 /*function $(selector){
   return document.querySelector(selector);
 }*/
 
-
 function other(text_edit){
-  //alert("LOL!");
-  /*other1_text*/text_edit.focus();
-  /*var other = document.getElementById('other');
-  var other1_text = document.getElementById('other1_text');
-  other1_text.focus();*/
+  text_edit.focus();
 }
+
+function size1(){
+  size = document.getElementById("size_input").value;
+  document.getElementById("size_output").innerHTML = size;
+  
+  for (var index = 0; index < 7; index++){
+    if(parseInt(document.getElementsByClassName("site-type").item(index).id) <= size)
+      document.getElementsByClassName("site-type").item(index).style.background = "rgba(0,175,0,0.6)";//"#00AF00";
+    else{
+      document.getElementsByClassName("site-type").item(index).style.background = "rgba(255,255,255,1)";
+      document.getElementsByClassName("site-type").item(index).style.background = "rgba(175,0,0,0.6)";
+      }
+  }
+}
+
+function set_size(size){
+  document.getElementById("size_input").value = size;
+  size1();
+}
+
